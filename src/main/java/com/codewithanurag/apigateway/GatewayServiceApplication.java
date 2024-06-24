9b62a82f2d9e1b6ec73730a92c516cf861807178
anuragsknsits <anuragsknsits@gmail.com>
2024-06-24 18:20:29 +0530

    Cors filter added it globally.
    Signed-off-by: anuragsknsits <anuragsknsits@gmail.com>

--- src/main/java/com/codewithanurag/apigateway/GatewayServiceApplication.java
+++ src/main/java/com/codewithanurag/apigateway/GatewayServiceApplication.java
@@ -1,11 +1,14 @@
 package com.codewithanurag.apigateway;
 
+import com.codewithanurag.apigateway.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(WebSecurityConfig.class)
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/codewithanurag/apigateway/config/WebSecurityConfig.java
+++ src/main/java/com/codewithanurag/apigateway/config/WebSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,4 +17,17 @@ public class WebSecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable).build();
     }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
 }
